e311d8acb06c515b9d97af55fefa4c1bbc70e964
Your Name <you@example.com>
2025-06-28 22:06:24 +0530

    Adding api to get Product

---
+++ src/main/java/dev/saransh/springbootapp_backend/SpringBootAppBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.saransh.springbootapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/saransh/springbootapp_backend/SpringBootAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saransh.springbootapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
