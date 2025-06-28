80cade072e7da47674beff1a6e07ea996059a62a
Your Name <you@example.com>
2025-06-29 00:48:37 +0530

    Added api for getting All Products

--- src/main/java/dev/saransh/springbootapp_backend/services/FakeStoreProductService.java
+++ src/main/java/dev/saransh/springbootapp_backend/services/FakeStoreProductService.java
@@ -2,14 +2,14 @@ package dev.saransh.springbootapp_backend.services;
 
 import dev.saransh.springbootapp_backend.dtos.FakeStoreProductDto;
 import dev.saransh.springbootapp_backend.dtos.ProductRequestDto;
-import dev.saransh.springbootapp_backend.dtos.WrapperForFakeStoreProductService;
 import dev.saransh.springbootapp_backend.models.Product;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,9 +22,15 @@ public class FakeStoreProductService implements ProductService {
     }
     @Override
     public List<Product> getAllProducts() {
-        ResponseEntity<WrapperForFakeStoreProductService> responseEntity = restTemplate.exchange("https://fakestoreapi.com/products", HttpMethod.GET,null, WrapperForFakeStoreProductService.class);
-        WrapperForFakeStoreProductService ad =  responseEntity.getBody();
-        return ad.toProductList();
+        ResponseEntity<List<FakeStoreProductDto>> responseEntity = restTemplate.exchange("https://fakestoreapi.com/products", HttpMethod.GET,null, new ParameterizedTypeReference<List<FakeStoreProductDto>>() {});
+//        WrapperForFakeStoreProductService ad =  responseEntity.getBody();
+        List<FakeStoreProductDto> list = responseEntity.getBody();
+        List<Product> newList = new ArrayList<>();
+        for (FakeStoreProductDto fakeStoreProductDto : list) {
+            newList.add(fakeStoreProductDto.toProduct());
+        }
+//        System.out.println(list);
+        return newList;
 
     }
 
